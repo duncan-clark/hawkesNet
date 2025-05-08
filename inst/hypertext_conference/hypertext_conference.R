f755e6e9396dce194eaf12043c3601ac6f44001b
duncan-clark <duncan-clark@hotmail.co.uk>
2025-05-08 10:00:15 -0400

    changed init params

--- inst/hypertext_conference/hypertext_conference.R
+++ inst/hypertext_conference/hypertext_conference.R
@@ -185,8 +185,11 @@ net <- dat_clean$net
 hist(dat_clean$times,breaks = 100)
 TRUNCATION <- net %n% 'n'
 
-# fit the model to the network:
-params_init <- list(mu = 100,
+# turns out initial param matter quite a bit:
+# if nodes form in [0,0.1] mu = 100 we expecet 10 events -> need node_lambda = 10
+# but this will never work almost all nodes are added as singletons 
+# need mu ~1000, node_lambda ~ 1
+params_init <- list(mu = 1000,
                    beta_overall = 0.1,
                    K = 0.9,
                    beta_edges = 0.1,
