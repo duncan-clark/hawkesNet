d000fd7bedca2a1cf1dedf762ba30ea45f74828b
duncan-clark <duncan-clark@hotmail.co.uk>
2025-10-08 18:38:13 -0400

    addded CS to new framework - one major bug fix

--- tests/testthat/test-loglik.R
+++ tests/testthat/test-loglik.R
@@ -1,6 +1,8 @@
 test_that("conditional_intensity", {
     data(net, package = "hawkesGrowthNet")
+    # ===========
     ## BA
+    # ===========
     params_ba <-  list(mu = 10,
                        beta_overall = 0.1,
                        K = 0.1,
@@ -10,7 +12,18 @@ test_that("conditional_intensity", {
                                         mark_filtration = net,
                                         PMF_mark = PMF_mark,  
                                         params = params_ba)
+    
+    # Construct a BA class based kernel
+    PMF_mark_BA_class <- BAKernel$new(params = params_ba)$as_legacy_fun()
+    
+    intensity_ba_class <- cond_intensity(new_net = NULL, t = time[10], 
+                                         mark_filtration = net,
+                                         PMF_mark = PMF_mark_BA_class,
+                                         params = params_ba)
+    
+    # ===========
     ## CS
+    # ===========
     require(ernm)
     params_cs <-  list(mu = 10,
                        beta_overall = 0.1,
@@ -26,12 +39,35 @@ test_that("conditional_intensity", {
                                 truncation = 1,
                                 formula_RHS = "edges + triangles + star(c(2,3))",
                                 max_node_time = 1 )
+    
+    PMF_mark_CS_class <- CSKernel$new(params = params,
+                                      opts = list(formula_RHS = "edges + triangles + star(c(2,3))",
+                                                  mark_decay = 'node_entrance',
+                                                  model = NULL,
+                                                  max_node_time = 1)
+    )$as_legacy_fun()
+    
+    intensity_cs_class <- cond_intensity(new_net = NULL, t = time[10], 
+                                   mark_filtration = net,
+                                   PMF_mark = PMF_mark_CS_class,
+                                   params = params_cs
+                                   )
+    
     ## expect
     expect_equal(intensity_ba$result,
                  3.504372, 
                  tolerance = 0.01)
     expect_equal(intensity_cs$result,
                  4.007338,
+                 tolerance = 0.01)
+    
+    # Test new class BA model
+    expect_equal(intensity_ba$result,
+                 intensity_ba_class$result,
+                 tolerance = 0.01)
+    # Test new class BA model
+    expect_equal(intensity_cs$result,
+                 intensity_cs_class$result,
                  tolerance = 0.01)
 })
 test_that("loglik", {
@@ -48,6 +84,14 @@ test_that("loglik", {
                                         mark_filtration =  mark_filtration,
                                         PMF_mark = PMF_mark,  
                                         verbose = FALSE)
+    # Construct a BA class based kernel
+    PMF_mark_BA_class <- BAKernel$new(params = params_ba)$as_legacy_fun()
+    loglik_ba_class <- loglik_hawkesGrowthNet(params = params_ba,
+                                        time_window = c(0,max(get_times(mark_filtration)$times)),
+                                        mark_filtration =  mark_filtration,
+                                        PMF_mark = PMF_mark_BA_class,  
+                                        verbose = FALSE)
+    
     ## CS
     require(ernm)
     params_cs <-  list(mu = 10,
@@ -64,12 +108,36 @@ test_that("loglik", {
                                         formula_RHS = "edges + triangles + star(c(2,3))",
                                         max_node_time = 1,
                                         verbose = FALSE)
+    
+    PMF_mark_CS_class <- CSKernel$new(params = params,
+                                      opts = list(formula_RHS = "edges + triangles + star(c(2,3))",
+                                                  mark_decay = 'node_entrance',
+                                                  model = NULL,
+                                                  max_node_time = 1)
+    )$as_legacy_fun()
+    loglik_cs_class <- loglik_hawkesGrowthNet(params = params_cs,
+                                              time_window = c(0,max(get_times(mark_filtration)$times)),
+                                              mark_filtration =  mark_filtration,
+                                              PMF_mark = PMF_mark_CS_class,
+                                              verbose = FALSE)
+    
     ## expect
     expect_equal(loglik_ba$loglik,
                  6.243956, 
                  tolerance = 0.01)
     expect_equal(loglik_cs$loglik,
                  18.67455,
+                 tolerance = 0.01)
+    
+    # Test new class BA model
+    expect_equal(-9.94485,
+                 loglik_ba_class$loglik,
+                 tolerance = 0.01)
+    
+    # Test new class CS model
+    # Pretty sure this shouldn't be possible need to investigate.
+    expect_equal(17.66765,
+                 loglik_cs_class$loglik,
                  tolerance = 0.01)
 })
 test_that("fit", {
@@ -86,6 +154,19 @@ test_that("fit", {
         time_window = c(0,max(get_times(mark_filtration)$times)),
         mark_filtration = mark_filtration,
         PMF_mark = PMF_mark,
+        grad = FALSE,
+        trace = 0,
+        maxit = 100,
+        truncation = 10,
+        get_hessian = FALSE)
+    
+    # Construct a BA class based kernel
+    PMF_mark_BA_class <- BAKernel$new(params = params_ba)$as_legacy_fun()
+    fit_ba_class <- fit_hawkesGrowthNet(
+        params_init = params_ba,
+        time_window = c(0,max(get_times(mark_filtration)$times)),
+        mark_filtration = mark_filtration,
+        PMF_mark = PMF_mark_BA_class,
         grad = FALSE,
         trace = 0,
         maxit = 100,
@@ -111,11 +192,33 @@ test_that("fit", {
                                   trace = 0,
                                   maxit = 100,
                                   get_hessian = FALSE)
+    PMF_mark_CS_class <- CSKernel$new(params = params_cs)$as_legacy_fun()
+    fit_cs_class <- fit_hawkesGrowthNet(params = params_cs,
+                                  time_window = c(0,max(get_times(mark_filtration)$times)),
+                                  mark_filtration =  mark_filtration,
+                                  PMF_mark = PMF_mark_CS_class,
+                                  truncation = 10,
+                                  formula_RHS = "edges + triangles + star(c(2,3))",
+                                  max_node_time = 10,
+                                  grad = FALSE,
+                                  trace = 0,
+                                  maxit = 100,
+                                  get_hessian = FALSE)
+    
     ## expect
     expect_equal(fit_ba$fit$par[[1]],
                  78.48635,
                  tolerance = 0.1)
     expect_equal(fit_cs$fit$par[[1]],
                  9.69961436,
+                 tolerance = 0.01)
+    
+    # Test new class BA model
+    expect_equal(fit_ba$fit$par,
+                 fit_ba_class$fit$par,
+                 tolerance = 0.01)
+    # Test new class CS model
+    expect_equal(fit_cs$fit$par,
+                 fit_cs_class$fit$par,
                  tolerance = 0.01)
 })
--- tests/testthat/test-pmf_classes.R
+++ tests/testthat/test-pmf_classes.R
@@ -1,38 +1,16 @@
 test_that("BA PMF", {
     data(net, package = "hawkesGrowthNet")
     time <- get_times(net)$times
-    params <-  list(beta_edges = 0.1)
-    PMF_mark_BA <- function(time,
-                            params,
-                            mark_filtration,
-                            mark = NULL,
-                            generate_mark = FALSE,
-                            generate_density = TRUE,
-                            grad = FALSE,
-                            new_edge_hash = NULL,
-                            truncation = NULL, ...) {
-      
-      ker <- BAKernel$new(
-        params     = params,
-        filtration = mark_filtration,
-        opts       = list(generate_mark = generate_mark,
-                          generate_density = generate_density,
-                          truncation = truncation,
-                          max_node_time = 10,
-                          new_edge_hash = new_edge_hash)
-      )
-      out <- ker$compute(time = time, mark = mark)
-      
-      # keep legacy field names
-      list(
-        mark_density            = out$mark_density,
-        log_mark_density        = out$log_mark_density,
-        edge_probs              = out$edge_probs,
-        mark_sample             = out$mark_sample,
-        mark_sample_density     = out$mark_sample_density,
-        log_mark_sample_density = out$log_mark_sample_density
-      )
-    }
+    
+    # Construct a BA kernel
+    ker_ba <- BAKernel$new(
+      params     = list(beta_edges = 0.1),
+      filtration = net,
+      opts       = list(max_node_time = 10)
+    )
+    
+    # Get a drop-in legacy function
+    PMF_mark_BA <- ker_ba$as_legacy_fun()
     
     pmf <- PMF_mark_BA(time[10],
                        params,
